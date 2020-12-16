b8d4db3b506324b220b4e0edec3739bb8d1a36c6
Christoph Landgraf <christoph.landgraf@contact-software.com>
2020-12-16 17:24:53 +0100

    Parse simple strings, improved error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,57 +1,55 @@
-use logos::{Logos, Lexer};
 use std::fmt;
 
-#[derive(Logos, Debug, PartialEq)]
-enum Token {
-    #[token("(")]
-    LBrace,
-    #[token(")")]
-    RBrace,
-    #[regex("[a-zA-Z]+", |lex| lex.slice().to_string())]
-    Symbol(String),
-    #[error]
-    #[regex(r"[ \t\n\f]+", logos::skip)]
-    Error,
-}
+mod lexer;
+
+use crate::lexer::{Tokens, ObjectT, StringT, Lexer};
 
 #[derive(Clone)]
 pub enum SExp {
     Symbol(String),
+    String(String),
+    Number(f64),
     List(Vec<SExp>),
 }
 
 const UNKNOWN_CHAR: &str = "Unexpected character.";
 const UNEXPECTED_RBRACE: &str = "Right brace without matching lbrace.";
+const UNEXPECTED_ENDOFSTR: &str = "Unexpected end of input while parsing string.";
+const INTERNAL_ERROR: &str = "Internal Error.";
 
-pub enum ParseError {
-    UnknownCharacter,
-    UnexpectedRbrace,
+pub enum ReadError {
+    UnknownCharacter((usize, usize)),
+    UnexpectedRbrace((usize, usize)),
+    UnexpectedEndOfString,
+    InternalError,
 }
 
-impl fmt::Display for ParseError {
+impl fmt::Display for ReadError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", match self {
-            ParseError::UnknownCharacter => UNKNOWN_CHAR,
-            ParseError::UnexpectedRbrace => UNEXPECTED_RBRACE,
+            ReadError::UnknownCharacter(_) => UNKNOWN_CHAR,
+            ReadError::UnexpectedRbrace(_) => UNEXPECTED_RBRACE,
+            ReadError::UnexpectedEndOfString => UNEXPECTED_ENDOFSTR,
+            ReadError::InternalError => INTERNAL_ERROR,
         })
     }
 }
 
-pub struct Parser {
-    stack: Vec<Vec<SExp>>,
+pub struct Reader {
+    stack: Vec<Vec<SExp>>
 }
 
-impl Parser {
-    pub fn new() -> Parser {
-        Parser {
+impl Reader {
+    pub fn new() -> Reader {
+        Reader {
             stack: vec![]
         }
     }
 
-    pub fn partial(&mut self, prog: &mut Vec<SExp>, input: &String) -> Result<(), ParseError> {
-        let mut lex = Token::lexer(&input[..]);
+    pub fn partial(&mut self, prog: &mut Vec<SExp>, input: &String) -> Result<(), ReadError> {
+        let mut lexer = Lexer::new(input);
         loop {
-            match self.parse_sexp(&mut lex) {
+            match self.parse_sexp(&mut lexer) {
                 Ok(Some(sexp)) => prog.push(sexp),
                 Ok(None) => return Ok(()),
                 Err(s) => return Err(s),
@@ -59,31 +57,69 @@ impl Parser {
         }
     }
 
-    fn parse_sexp(&mut self, lexer: &mut Lexer<Token>) -> Result<Option<SExp>, ParseError> {
+    fn parse_sexp(&mut self, lexer: &mut Lexer) -> Result<Option<SExp>, ReadError> {
         loop {
             match lexer.next() {
-                Some(Token::Error)
-                    => return Err(ParseError::UnknownCharacter),
+                Some(Tokens::String(_))
+                    => return Err(ReadError::InternalError),
+                Some(Tokens::Object(ObjectT::Error))
+                    => return Err(ReadError::UnknownCharacter(lexer.span())),
+
                 None
                     => return Ok(None),
-                Some(Token::LBrace)
+
+                Some(Tokens::Object(ObjectT::LBrace))
                     => self.stack.push(vec![]),
-                Some(Token::RBrace)
+                Some(Tokens::Object(ObjectT::RBrace))
                     => match self.stack.len() {
-                        0 => return Err(ParseError::UnexpectedRbrace),
+                        0 => return Err(ReadError::UnexpectedRbrace(lexer.span())),
                         1 => return Ok(Some(self.pop_list())),
                         _ => {
                             let sxp = self.pop_list();
                             self.push_sexp(sxp);
                         }
                     },
-                Some(Token::Symbol(s))
-                    => match self.stack.len() {
-                        0 => return Ok(Some(SExp::Symbol(s))),
-                        _ => self.push_sexp(SExp::Symbol(s))
+                Some(Tokens::Object(ObjectT::Number(n)))
+                    => if let Some(a) = self.handle_atom(SExp::Number(n)) {
+                        return Ok(Some(a))
                     },
+                Some(Tokens::Object(ObjectT::Symbol(s)))
+                    => if let Some(a) = self.handle_atom(SExp::Symbol(s)) {
+                        return Ok(Some(a))
+                    },
+                Some(Tokens::Object(ObjectT::StartString))
+                    => return self.parse_string(lexer).map(|s| Some(s)),
             }
         }
+    }
+
+    fn handle_atom(&mut self, a: SExp) -> Option<SExp> {
+        match self.stack.len() {
+            0 => return Some(a),
+            _ => self.push_sexp(a)
+        }
+        None
+    }
+
+    fn parse_string(&mut self, lexer: &mut Lexer) -> Result<SExp, ReadError> {
+        let mut string = String::new();
+        let res = loop {
+            match lexer.next() {
+                Some(Tokens::Object(_))
+                    => return Err(ReadError::InternalError),
+                Some(Tokens::String(StringT::Error))
+                    => return Err(ReadError::UnknownCharacter(lexer.span())),
+
+                None
+                    => break Err(ReadError::UnexpectedEndOfString),
+
+                Some(Tokens::String(StringT::Text(s)))
+                    => string.push_str(&s[..]),
+                Some(Tokens::String(StringT::EndString))
+                    => break Ok(()),
+            }
+        };
+        res.map(|()| SExp::String(string))
     }
 
     fn push_sexp(&mut self, sexp: SExp) {
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,50 @@
+use ansi_term::Colour::{Blue, Red};
 use rustyline::{error::ReadlineError, Editor};
 use rustyline;
+use std::fmt;
 
-use lisp::{Parser, SExp};
+use lisp::{Reader, ReadError, SExp};
+
+fn print_range_error(input: &String, err: ReadError, start: usize, end: usize) {
+    print_error_msg(&err);
+    eprintln!(" {} {}",   Blue.paint("|"), input);
+    eprintln!(" {} {}{}", Blue.paint("|"), " ".repeat(start), Red.paint("^".repeat(end - start)));
+}
+
+fn print_error_msg(displayable: &dyn fmt::Display) {
+    eprintln!("{}: {}", Red.paint("Error"), displayable);
+}
+
+fn handle_error(input: &String, err: ReadError) -> Result<(), ReadError> {
+    match err {
+        ReadError::UnknownCharacter((start, end)) => print_range_error(input, err, start, end),
+        ReadError::UnexpectedRbrace((start, end)) => print_range_error(input, err, start, end),
+        ReadError::UnexpectedEndOfString => print_error_msg(&ReadError::UnexpectedEndOfString),
+        ReadError::InternalError => return Err(ReadError::InternalError),
+    }
+    Ok(())
+}
 
 fn main() {
     let mut rl = Editor::<()>::new();
     let mut prog: Vec<SExp> = vec![];
-    let mut parser: Parser = Parser::new();
+    let mut parser: Reader = Reader::new();
 
     loop {
         let prompt = match parser.len() {
             0 => "? ".to_string(),
-            _ => format!("  {}", "> ".repeat(parser.len())),
+            _ => format!("> {}", "  ".repeat(parser.len())),
         };
 
         match rl.readline(&prompt[..]) {
-            Ok(line) => match parser.partial(&mut prog, &line) {
-                Ok(()) => (),
-                Err(e) => break Err(e.to_string()),
+            Ok(line) =>  if let Err(e) = parser.partial(&mut prog, &line) {
+                if let Err(e) = handle_error(&line, e) {
+                    break Err(e.to_string());
+                }
             },
             Err(ReadlineError::Eof)         => break Ok(()),
             Err(ReadlineError::Interrupted) => break Ok(()),
             Err(e) => break Err(e.to_string()),
         }
-    }.unwrap_or_else(|err| eprintln!("Error in reader: {}", err));
+    }.unwrap_or_else(|err| print_error_msg(&err));
 }
